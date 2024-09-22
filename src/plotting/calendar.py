1967f2a672fbfe0ad459900ac1b696b82113bbaa
Sander Van Aken <sander.van-aken@flixbus.com>
2024-09-22 19:42:44 +0200

    test calendar plot

--- src/plotting/calendar.py
+++ src/plotting/calendar.py
@@ -4,22 +4,22 @@ import pandas as pd
 import calplot
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import matplotlib.patches as mpatches
 
 
 __COLORMAP = colors.ListedColormap(['red', 'green', 'yellow'])
 
 
-def holiday_calendar_plot(bank_holidays: list[date], weekend_days: list[date], days_off: list[date]) -> None:
-    bank_holiday_series = pd.Series(100, index=bank_holidays)
+def holiday_calendar_plot(public_holidays: list[date], weekend_days: list[date], days_off: list[date]) -> None:
+    public_holiday_series = pd.Series(1, index=public_holidays)
 
-    weekend_days = list(set(weekend_days) - set(bank_holidays))
-    print(weekend_days)
-    weekend_days_series = pd.Series(300, index=weekend_days)
+    weekend_days = list(set(weekend_days) - set(public_holidays))
+    weekend_days_series = pd.Series(3, index=weekend_days)
 
-    days_off = list(set(days_off) - set(weekend_days) - set(bank_holidays))
-    days_off_series = pd.Series(200, index=days_off)
+    days_off = list(set(days_off) - set(weekend_days) - set(public_holidays))
+    days_off_series = pd.Series(2, index=days_off)
 
-    combined = pd.concat([bank_holiday_series, days_off_series, weekend_days_series])
+    combined = pd.concat([public_holiday_series, days_off_series, weekend_days_series])
     combined.index = combined.index.astype('datetime64[ns]')
 
     calplot.calplot(
@@ -29,7 +29,13 @@ def holiday_calendar_plot(bank_holidays: list[date], weekend_days: list[date], d
         linewidth=0.5,
         edgecolor='red',
         colorbar=False,
-        dropzero=True
+        dropzero=True,
     )
+
+    public_holiday_patch = mpatches.Patch(color='red', label='Public Holiday')
+    weekend_days_patch = mpatches.Patch(color='yellow', label='Weekend')
+    days_off_patch = mpatches.Patch(color='green', label='Day Off')
+
+    plt.legend(handles=[public_holiday_patch, weekend_days_patch, days_off_patch], loc='right', bbox_to_anchor=(1.22, 1))
 
     plt.savefig('calendar_plot.png')
